1b95c8b051b68b05c243c349a66bab7b4c7a8860
Tit <jing_yunti@163.com>
2021-01-21 13:16:05 +0800

    dev分支初始化操作结束

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -4,6 +4,5 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("第一次操作添加暂存区！");
         System.out.println("dev分支初始化操作");
-        System.out.println("dev初始化分支操作结束！");
     }
 }
