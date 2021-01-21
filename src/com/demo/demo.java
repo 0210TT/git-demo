a1e7ced04445004c0a761ed80eb12aae479a1d2f
Tit <jing_yunti@163.com>
2021-01-21 13:19:33 +0800

    主干初始化操作结束

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("第一次操作添加暂存区！");
         System.out.println("dev分支初始化操作");
+        System.out.println("主干初始化分支操作！");
     }
 }
